19c08487aefb661d15fbb4426b9de4bf26b0a5ae
Mihai Pantiru <mihai.pantiru@gmail.com>
2020-06-16 09:03:24 +0300

    Add basic routes for Form and Register

--- Sources/App/Controllers/FormController.swift
+++ Sources/App/Controllers/FormController.swift
@@ -4,5 +4,29 @@ import Fluent
 
 final class FormController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
+        let formRoute = routes.grouped("api", "form")
+        formRoute.post(use: createHandle)
+        formRoute.get(use: getAllHandler)
+        formRoute.delete(":formID", use: deleteHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> EventLoopFuture<[FormResult]> {
+        return FormResult.query(on: req.db).all()
+    }
+    
+    func createHandle(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let formResult = try req.content.decode(FormResult.self)
+        return formResult.create(on: req.db).transform(to: HTTPStatus.ok)
+    }
+    
+    func deleteHandler(_ req: Request)
+        throws -> EventLoopFuture<HTTPStatus> {
+            let itemToDelete = FormResult.find(req.parameters.get("formID"), on: req.db)
+                .unwrap(or: Abort(.notFound))
+            let status = itemToDelete.flatMap { item -> EventLoopFuture<Void> in
+                return item.delete(on: req.db)
+            }
+            
+            return status.transform(to: HTTPStatus.ok)
     }
 }
--- Sources/App/Controllers/RegisterController.swift
+++ Sources/App/Controllers/RegisterController.swift
@@ -4,5 +4,29 @@ import Fluent
 
 final class RegisterController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
+        let registerRoute = routes.grouped("api", "register")
+        registerRoute.post(use: createHandle)
+        registerRoute.get(use: getAllHandler)
+        registerRoute.delete(":userID", use: deleteHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> EventLoopFuture<[Student]> {
+        return Student.query(on: req.db).all()
+    }
+    
+    func createHandle(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let student = try req.content.decode(Student.self)
+        return student.create(on: req.db).transform(to: HTTPStatus.ok)
+    }
+    
+    func deleteHandler(_ req: Request)
+        throws -> EventLoopFuture<HTTPStatus> {
+            let itemToDelete = Student.find(req.parameters.get("userID"), on: req.db)
+                .unwrap(or: Abort(.notFound))
+            let status = itemToDelete.flatMap { item -> EventLoopFuture<Void> in
+                return item.delete(on: req.db)
+            }
+            
+            return status.transform(to: HTTPStatus.ok)
     }
 }
